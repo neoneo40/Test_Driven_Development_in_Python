66a91526fa1fe69f83af1e21548908154605c03b
re4lfl0w <re4lfl0w@gmail.com>
2015-04-13 16:49:53 +0900

    Refactoring urlParser.py

--- urlParser.py
+++ urlParser.py
@@ -1,7 +1,6 @@
 class urlParser:
     def __init__(self, url):
-        protocol, site_and_path = url.split('://')
-        self.protocol = protocol
+        self.protocol, site_and_path = url.split('://')
         site_and_path_list = site_and_path.split('/')
         self.site = site_and_path_list[0]
         self.path = '/'.join(site_and_path_list[1:])
