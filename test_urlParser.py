544ac0fbdb1bfd565c8ee4b3e556b909ad64864b
re4lfl0w <re4lfl0w@gmail.com>
2015-04-13 16:22:01 +0900

    Success http, ftp, other ptorocol

--- test_urlParser.py
+++ test_urlParser.py
@@ -10,3 +10,7 @@ class basicProtocolParsing(unittest.TestCase):
     def test_ftp_protocol(self):
         purl = urlParser('ftp://ftp.essex.ac.uk')
         self.assertEqual(purl.protocol, 'ftp')
+
+    def test_other_protocol(self):
+        purl = urlParser('w61xw://some.site.com')
+        self.assertEqual(purl.protocol, 'w61xw')
--- urlParser.py
+++ urlParser.py
@@ -1,6 +1,4 @@
 class urlParser:
     def __init__(self, url):
-        if url[0] == 'h':
-            self.protocol = 'http'
-        else:
-            self.protocol = 'ftp'
+        protocol, rest = url.split('://')
+        self.protocol = protocol
