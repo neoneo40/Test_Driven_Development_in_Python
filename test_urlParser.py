445ff70d29a816ce02ea7da751abc8d527497d3b
re4lfl0w <re4lfl0w@gmail.com>
2015-04-13 16:14:10 +0900

    test_protocol

--- test_urlParser.py
+++ test_urlParser.py
@@ -3,5 +3,7 @@ import unittest
 from urlParser import urlParser
 
 class basicProtocolParsing(unittest.TestCase):
-    pass
+    def test_protocol(self, ):
+        purl = urlParser('http://www.essex.ac.uk')
+        self.assertEqual(purl.protocol, 'http')
 
